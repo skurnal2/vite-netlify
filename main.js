973440c7f2b40ef118166ce1b3e5182e3b1910e0
【﻿Ｓｉｄ】 <49497542+skurnal2@users.noreply.github.com>
2024-03-29 11:35:26 -0600

    TEST

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ document.querySelector('#app').innerHTML = `
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
+    test
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
